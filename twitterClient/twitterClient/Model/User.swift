12520be62cb1a2c37d0c5710b095e00d86ccf4a2
anluferov <andrey.luferau@apalon-inc.com>
2019-12-19 22:09:32 +0300

    first commit

---
+++ twitterClient/twitterClient/Model/User.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  twitterClient
+//
+//  Created by AP Andrey Luferau on 12/19/19.
+//  Copyright © 2019 AP Andrey Luferau. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct User {
+    let name: String = ""
+    let userName: String = ""
+    let avatar: UIImage
+
+    let tweetMessage: String = ""
+    let dateMessage: NSDate
+}
